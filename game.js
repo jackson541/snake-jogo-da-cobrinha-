22cf76ef10b202a341f16e2fcc2149ab303a22fa
jackson541 <jacksonalves541@gmail.com>
2020-04-06 00:08:33 +0000

    teletransporte no limite da tela e movimento da fruta após colisão

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-export default function newGame () {
+export default function newGame (tela) {
 
     const snake = {
         parts: {
@@ -8,7 +8,10 @@ export default function newGame () {
         }
     }
 
-    const fruit = { x:5, y:6 }
+    //acontecer algum erro na rederização e por isso não consegui deixar apenas como um objeto com x e y
+    const fruits = {
+        'fruit': { x:5, y:6 }
+    }
 
     const state = {
         intervalMove: null,
@@ -22,29 +25,30 @@ export default function newGame () {
         
         const moves = {
             ArrowUp (part) {
-                part.y --
+                part.y = part.y - 1 < 0 ? tela.height - 1 : part.y - 1
                 part.move = 'ArrowUp'
             },
 
             ArrowDown (part) {
-                part.y ++
+                part.y = part.y + 1 >= tela.height ? 0 : part.y + 1
                 part.move = 'ArrowDown'
             },
 
             ArrowLeft (part) {
-                part.x --
+                part.x = part.x - 1 < 0 ? tela.width - 1 : part.x - 1
                 part.move = 'ArrowLeft'
             },
 
             ArrowRight (part) {
-                part.x ++
+                part.x = part.x + 1 >= tela.width ? 0 : part.x + 1
                 part.move = 'ArrowRight'
             },
         }
 
         function verifyColision (command) {
-            if (command.y === fruit.y && command.x === fruit.x) {
-                console.log('colidiu')
+            if (command.y === fruits['fruit'].y && command.x === fruits['fruit'].x) {
+                moveFruit()
+                state.valuePart ++
             }
         }
 
@@ -69,16 +73,16 @@ export default function newGame () {
 
 
     function moveFruit () {
-        fruit = {
-            x: Math.floor( Math.random * 20 ),
-            x: Math.floor( Math.random * 20 )
+        fruits['fruit'] = {
+            x: Math.floor( Math.random() * 20 ),
+            y: Math.floor( Math.random() * 20 )
         }
     }
 
     return {
         moveSnake,
         snake,
-        fruit
+        fruits
     }
 
 }
--- renderGame.js
+++ renderGame.js
@@ -4,18 +4,16 @@ export default function newRenderGame (tela, game) {
     context.clearRect(0, 0, 20, 20)
 
     const parts = game.snake.parts
-    const fruit = game.fruit
+    const fruit = game.fruits['fruit']
 
     for (const partId in parts) {
         const part = parts[partId]
         context.fillStyle = 'blue'
         context.fillRect(part.x, part.y, 1, 1)
     }
-
    
     context.fillStyle = 'green'
     context.fillRect(fruit.x, fruit.y, 1, 1)
-
 
     requestAnimationFrame(() => {
         newRenderGame(tela, game)
